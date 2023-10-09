5aef6615e8fd9dd0d981e250167a7b0493fbbb8e
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-10-09 21:09:39 +0300

    feat(): task №4 done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,55 @@
+class Key {
+  constructor(
+    private signature: number = parseFloat(Math.random().toFixed(2))
+  ) {}
+
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  public abstract openDoor(key: Key): boolean;
+
+  public comeIn(person: Person): void {
+    if (this.door) {
+      console.log(`The door is open`);
+      this.tenants.push(person);
+    } else {
+      console.log(
+        `The door is closed. The key ${key.getSignature()} are wrong`
+      );
+    }
+  }
+}
+
+class MyHouse extends House {
+  public openDoor(key: Key): boolean {
+    return (this.door = this.key === key);
+  }
+}
+
 const key = new Key();
 
-const house = new MyHouse(key);
 const person = new Person(key);
+
+const house = new MyHouse(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
